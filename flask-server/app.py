bfb2d128c238984777c72438bc2441fca88fc204
Sam <90927709+sAm2097@users.noreply.github.com>
2022-07-16 14:34:10 +0200

    Update app.py

--- flask-server/app.py
+++ flask-server/app.py
@@ -1,10 +1,155 @@
+from asyncio.windows_events import NULL
 import os
+from urllib import response
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 from flask import send_from_directory
 import uuid
 
-from sqlalchemy import false
+
+
+Jsonfile={	
+
+	"Dekor_Name": "3236_gedreht",
+	"image_source_dir": "/media/software_new/Oberflaechen Inspektion/Datenbank/Kundenmuster/Ballerina/3236 Keramik dunkel/3236_gedreht",
+	"image_source_dir_cut": False,
+	"train_split": 85,	
+	"Number_of_epochs": 85,
+	"sliced_error_image_dir": [
+		"/media/software_new/Oberflaechen Inspektion/Datenbank/Fehlerkatalog",
+		"/media/software_new/Oberflaechen Inspektion/Datenbank/Fehlerkatalog/Ballerina_ausgeschnittene Fehler"
+	],
+	"real_errorplates_dir": "/media/software_new/Oberflaechen Inspektion/Datenbank/Kundenmuster/Ballerina/3236 Keramik dunkel/Fehlerbilder",
+	"erg_pfad": "/home/knecht/Dokumente/Lokale_Datenbank/Ballerina_Training/3236_gedreht/",
+	"defektor_train": {
+		"plates": 0,
+		"Fehler": 0,
+		"global_plates_mean": -1.0,
+		"global_plates_variance": -1.0,
+		"stride_ratio_width": 0.5,
+		"stride_ratio_height": 0.5,
+		"plates_scale": 0.8,
+		"target_width": 512,
+		"target_height": 512,
+		"rotationRangeDeg": 0.2,
+		"cuttingRange": [
+			300,
+			300
+		],
+		"mirror": True,
+		"rotate_90": False,
+		"patch_brightnessMin": -15,
+		"patch_brightnessMax": 15,
+		"useNoiseOnGT": False,
+		"useBrightnessOnGT": True,
+		"threshold_find_errors": 5,
+		"minimum_error_contour_size": 100,
+		"minimum_error_size": 50,
+		"error_scale_lower_range": 0.7,
+		"error_scale": 0.9,
+		"borderDistance": 20,
+		"alphaMin": 5,
+		"alphaMax": 20,
+		"sigmaMin": 15,
+		"sigmaMax": 20,
+		"error_brightnessMin": 0.85,
+		"error_brightnessMax": 1.15,
+		"placingRate": [
+			25,
+			30,
+			30,
+			10,
+			5
+		],
+		"Scalenorm_sigma": 5.326724,
+		"GaussianBlur_sigmaX": 0.0,
+		"__class__": "defektor"
+	},
+	"defektor_val": {
+		"plates": 0,
+		"Fehler": 0,
+		"global_plates_mean": -1.0,
+		"global_plates_variance": -1.0,
+		"stride_ratio_width": 0.5,
+		"stride_ratio_height": 0.5,
+		"plates_scale": 0.8,
+		"target_width": 512,
+		"target_height": 512,
+		"rotationRangeDeg": 0.2,
+		"cuttingRange": [
+			300,
+			300
+		],
+		"mirror": True,
+		"rotate_90": False,
+		"patch_brightnessMin": -15,
+		"patch_brightnessMax": 15,
+		"useNoiseOnGT": False,
+		"useBrightnessOnGT": True,
+		"threshold_find_errors": 5,
+		"minimum_error_contour_size": 100,
+		"minimum_error_size": 50,
+		"error_scale_lower_range": 0.7,
+		"error_scale": 0.9,
+		"borderDistance": 20,
+		"alphaMin": 5,
+		"alphaMax": 20,
+		"sigmaMin": 15,
+		"sigmaMax": 20,
+		"error_brightnessMin": 0.85,
+		"error_brightnessMax": 1.15,
+		"placingRate": [
+			25,
+			30,
+			30,
+			10,
+			5
+		],
+		"Scalenorm_sigma": 5.326724,
+		"GaussianBlur_sigmaX": 0.0,
+		"__class__": "defektor"
+	},
+	"model_class": {
+		"width": 512,
+		"height": 512,
+		"batchsize": 5,
+		"GPU_Memory": 8,
+		"n_filters": [
+			32,
+			64,
+			128,
+			256,
+			512,
+			1024
+		],
+		"conv_padding": "same",
+		"conv_activation": "sigmoid",
+		"dropout": [
+			0.1,
+			0.1,
+			0.1,
+			0.1,
+			0.1,
+			0.1
+		],
+		"Batchnorm": True,
+		"VAE_option": NULL,
+		"inital_kl_weight": 0.1,
+		"train_systematik": 1,
+		"Framework_name": "U-Net\"2\"",
+		"model_name": "U-Net-2",
+		"Layer_INPUT_NAME": "input",
+		"Layer_OUTPUT_NAME": "praed/Sigmoid",
+		"__class__": "U_Net_2"
+	},
+	"my_callback": {
+		"erg_pfad": "/home/knecht/Dokumente/Lokale_Datenbank/Ballerina_Training/3236_gedreht/",
+		"use_Tensorboard": False,
+		"__class__": "segment_Plates"
+	},
+	 "__class__": "onclass_learner"
+}
+
 
 
 Tasks= [
@@ -13,8 +158,8 @@ Tasks= [
           'title': 'Task1',
           'description': 'About trained task',
           'folder':'folder_1',
-          'X':'1',
-          'Y':'0.9',
+        #   'X':'1',
+        #   'Y':'0.9',
           'read':True 
         },
      ]
@@ -39,8 +184,9 @@ def all_tasks():
             'title': post_data.get('title'),
             'description': post_data.get('description'),
             'folder':post_data.get('folder'),
-            'X':post_data.get('X'),
-            'Y':post_data.get('Y'),
+			'par':post_data.get('par'),
+            # 'X':post_data.get('X'),
+            # 'Y':post_data.get('Y'),
             
            'read': post_data.get('read'),
         })
@@ -62,8 +208,8 @@ def single_task(task_id):
             'title': post_data.get('title'),
             'description': post_data.get('description'),
             'folder':post_data.get('folder'),
-            'X':post_data.get('X'),
-            'Y':post_data.get('Y'),
+            # 'X':post_data.get('X'),
+            # 'Y':post_data.get('Y'),
            'read': post_data.get('read'),
         })
         response_object['message'] = 'Task updated!'
@@ -81,11 +227,15 @@ def remove_task(task_id):
 
 @app.route('/tasks/uploaded_files',methods=['GET'])
 def SelectFolder():
-    
     folders=os.listdir(request.args['path'])
     return {"data":folders}
 
-   
+
+
+@app.route('/tasks/parameters',methods=['GET'])   
+def paramsFile():
+    return jsonify(Jsonfile)
+
 
 
      
